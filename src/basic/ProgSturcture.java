9c86342cb5f8d750bd0b733a5f7380a39207c8f1
pravin-singh-parihar <parihar.pravinsingh772005@gmail.com>
2024-03-17 23:52:35 +0530

    first commit

---
+++ src/basic/ProgSturcture.java
@@ -0,0 +1,15 @@
+package basic;
+//An package is the collection of the class or interface
+
+
+import java.util.Scanner;
+//import statement are the import the predifine classes and intrface
+public class ProgSturcture {
+    //class is the bluprint it is the collection of the method,object and variable
+    public static void main(String[] args) {
+        // it is the main method and it is statrting point of the jvm we can't run the program without a main method
+
+      /*Variable declare*/int a=10;// this part is the is variable decleration
+        // it is the varible
+    }
+}
